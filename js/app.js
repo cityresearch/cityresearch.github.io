17f47b0b062a9ee69bcd9721278f0a965b3b11c7
Patrick Smith <communityresearch@gmail.com>
2015-02-13 18:45:48 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var map;
 function main() {
 
   var options = {
-    center: [38.2500, -85.766],
+    center: [-42.27730877423707, 172.63916015625],
     zoom: 6, 
     zoomControl: false,  // dont add the zoom overlay (it is added by default)
     loaderControl: false //dont show tiles loader
