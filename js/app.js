a559b2a8bcae8e85b1ec9f710eac5b91e2b5b4ec
Patrick Smith <communityresearch@gmail.com>
2015-02-20 16:35:36 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,6 +10,29 @@ function main() {
   };
 
   cartodb.createVis('map', 'http://cityresearch.cartodb.com/api/v2/viz/5c39067a-b3aa-11e4-a01f-0e9d821ea90d/viz.json', options)
-    
+        .done(function(vis, layers) {
+      // there are two layers, base layer and points layer
+      var sublayer = layers[1].getSubLayer(0);
+      sublayer.set({ 'interactivity': ['cartodb_id', 'name_to_display', 'description'] });
+
+ 
+
+      // add the tooltip show when hover on the point
+      vis.addOverlay({
+        type: 'tooltip',
+        position: 'top|center',
+        template: '<p>{{name_to_display}}</p>'
+      });
+
+      vis.addOverlay({
+        type: 'infobox',
+        template: '<h3>{{name_to_display}}</h3><p>{{description}}</p>',
+        width: 200,
+        position: 'bottom|right'
+      });
+
+    });
+
+}
 
 window.onload = main;
